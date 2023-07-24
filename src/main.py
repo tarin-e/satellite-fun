2fc7725ecbffbabab4d954cbdae0870a0aa84cd4
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-07-25 11:31:46 +1200

    correct main and class interaction

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from satellite_catcher import SatelliteCatcher
 import pandas as pd
+import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
 def main():
+    satellite_catcher = SatelliteCatcher()
+    sat = satellite_catcher.catch_sats("ISS")
+    print(sat)
 
 if __name__ == "__main__":
     main()
